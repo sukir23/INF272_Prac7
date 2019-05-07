43b965ffc3dcdaed44da71277d14a1f43ace4f7e
Miss. KB Sanangura <u18055062@up.ac.za>
2019-05-07 18:02:16 +0200

    new

---
+++ KSite.js
@@ -0,0 +1,4 @@
+function AlertUser()
+{
+	alert('I'm to excited to expand my knowledge of coding. I'm always up for a challenge and would like to learn something that will help me grow as a person');
+}
